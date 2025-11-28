8b4aa9b7ace478a5ee15c28efbb20ffad16c7b8e
0xzahed <zahed01x@gmail.com>
2025-11-28 16:56:11 +0600

    deployment uopdate

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
   images: {
     remotePatterns: [
@@ -11,6 +10,10 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co", 
       },
     ],
   },
